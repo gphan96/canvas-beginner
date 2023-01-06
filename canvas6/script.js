6dc03d897b8fce962ebd903a137c40c5824379d1
G Phan <phankiengiang96@gmail.com>
2023-01-06 20:49:59 +0100

    Update free assets

--- canvas6/script.js
+++ canvas6/script.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const characters = [1, 2, 3, 4, 5, 6, 7];
+canvas.width = 900;
+canvas.height = 400;
+
+const characters = [1, 2, 4, 5, 6, 7];
 const actions = ['/run/', '/roll/', '/jump/', '/KO/'];
 
-let assetPath = 'free_assets/' + characters[0] + actions[0];
+let assetPath = 'free_assets/' + characters[2] + actions[3];
 
 let images = [];
 let frame = 0;
@@ -27,7 +30,7 @@ function loadAssets() {
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(images[frame], 0, canvas.height - images[frame].height);
+	ctx.drawImage(images[frame], 100, canvas.height - images[frame].height - 50);
 	if (frame === images.length - 1) {
 		frame = 0;
 	}
