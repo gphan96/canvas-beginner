c4dd20e6b5433da9dc6aa44099ac36eb8e710232
G Phan <phankiengiang96@gmail.com>
2023-01-10 13:27:46 +0100

    New Canvas game with NodeJS

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 https://gphan96.github.io/canvas-beginner/canvas1/
 
+# 
 https://gphan96.github.io/canvas-beginner/canvas2/
 
+# 
 https://gphan96.github.io/canvas-beginner/canvas3/
 
+# 
 https://gphan96.github.io/canvas-beginner/canvas4/
 
-https://gphan96.github.io/canvas-beginner/canvas5/    - same as canvas3 but with an easier way of generating particles
+# 
+https://gphan96.github.io/canvas-beginner/canvas5/
+   - same as canvas3 but with an easier way of generating particles
 
-https://gphan96.github.io/canvas-beginner/canvas6/    - check out free assets at bevouliin.com
+# 
+https://gphan96.github.io/canvas-beginner/canvas6/
+   - run the server with nodejs
+   - check out free assets at bevouliin.com
--- canvas6/script.js
+++ canvas6/script.js
@@ -1,46 +1,105 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let select = document.getElementById('select');
 
-canvas.width = 900;
-canvas.height = 400;
+canvas.width = 1500;
+canvas.height = 550;
 
-const characters = [1, 2, 4, 5, 6, 7];
-const actions = ['/run/', '/roll/', '/jump/', '/KO/'];
-
-let assetPath = 'free_assets/' + characters[2] + actions[3];
-
-let images = [];
-let frame = 0;
-const frameRate = 30;
-
-function loadAssets() {
-	console.log(assetPath);
-	fetch('/dir?path=' + assetPath)
-		.then((response) => response.json())
-		.then((files) => {
-			for (let i = 0; i < files.length; i++) {
-				let image = new Image();
-				image.src = assetPath + (i + 1) + '.png';
-				images.push(image);
-			}
-			animate();
-		})
-		.catch((error) => console.error(error));
-}
-
-function animate() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(images[frame], 100, canvas.height - images[frame].height - 50);
-	if (frame === images.length - 1) {
-		frame = 0;
+class Background {
+	constructor() {
+		this.src = 'free_assets/backgroundLayers';
+		this.backgroundImages = [];
+		this.backgroundSpd = 0;
+		this.width = 1833;
+		this.speed = 0.7;
 	}
-	frame++;
-	
-	setTimeout(() => {
-		requestAnimationFrame(animate);
-	}, 1000 / frameRate);
+	loadBackground() {
+		fetch('/dir?path=' + this.src)
+			.then((response) => response.json())
+			.then((files) => {
+				for (let i = 1; i <= files.length; i++) {
+					let image = new Image();
+					image.src = this.src + '/layer-' + i + '.png';
+					this.backgroundImages.push(image);
+				}
+			})
+			.catch((error) => console.error(error));
+	}
+	animateBackground() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		this.backgroundImages.forEach((image, index) => {
+			let dx = index * index * this.speed * this.backgroundSpd % image.width;
+			ctx.drawImage(image, dx, canvas.height - image.height);
+			ctx.drawImage(image, dx + image.width - 1, canvas.height - image.height);
+		});
+	}
 }
+
+class Character {
+	constructor(id) {
+		this.id = id;
+		this.src = 'free_assets/character_' + this.id + '/';
+		this.actionImages = [];
+		this.frame = 0;
+		this.action;
+		this.animate;
+		this.previousFrameTime = 0;
+		this.background = new Background();
+	}
+	run() {
+		this.action = 'run';
+		this.background.loadBackground();
+		this.loadAction()
+			 .then(() => {
+					this.animateAction(0);
+			 })
+			 .catch((error) => console.error(error));
+	}
+	loadAction() {
+		return fetch('/dir?path=' + this.src + this.action)
+			.then((response) => response.json())
+			.then((files) => {
+				for (let i = 1; i <= files.length; i++) {
+					let image = new Image();
+					image.src = this.src + this.action + '/' + i + '.png';
+					this.actionImages.push(image);
+				}
+			})
+	}
+	animateAction(currentTime) {
+		let eslapsedTime = currentTime - this.previousFrameTime;
+		if (eslapsedTime >= 1000 / FPS) {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			this.background.animateBackground();
+			ctx.drawImage(this.actionImages[this.frame], 300, canvas.height - this.actionImages[this.frame].height - 75);
+			if (this.frame === this.actionImages.length - 1) {
+				this.frame = 0;
+			}
+			this.frame++;
+			this.background.backgroundSpd--;
+			this.previousFrameTime = currentTime;
+		}
+		this.animate = requestAnimationFrame(this.animateAction.bind(this));
+	}
+	animateCancel() {
+		cancelAnimationFrame(this.animate);
+	}
+}
+
+const FPS = 30;
+let character;
 
 window.addEventListener('load', () => {
-	loadAssets();
+	character = new Character(select.value);
+	character.run();
+	console.log(character);
+})
+
+select.addEventListener('change', () => {
+	if(character) {
+		character.animateCancel();
+	}
+	character = new Character(select.value);
+	character.run();
+	console.log(character);
 })
